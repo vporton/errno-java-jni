760fdb91f6de75c32a2ceadf5e93f283c9874722
Victor Porton <porton@narod.ru>
2018-01-29 22:19:20 +0200

    initial version

---
+++ src/Java_Errno.c
@@ -0,0 +1,8 @@
+// Native methods implementation of
+// /home/porton/Projects/errno-java/src/org/porton/errno/Errno.java
+
+#include "Java_Errno.h"
+
+jstring JNICALL Java_org_portonvictor_errno_Errno_getMessage
+  (JNIEnv * env, jobject object) {
+}
